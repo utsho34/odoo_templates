2949abb8a031fbadeafaca6271fd4e6f9bcc4d6f
Utsho-Bs23 <utsho.biswas@brainstation-23.com>
2023-09-21 16:58:36 +0600

    [ADD]module template added

---
+++ module_template/manifest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "",
+
+    'summary': """
+        """,
+
+    'description': """
+    """,
+
+    'author': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+    'data': [],
+
+    'installable': True,
+    'Application': True,
+
+}
